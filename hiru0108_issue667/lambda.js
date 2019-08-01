ba9a45927d600d8265920f3f6db4f9ba842b5bd6
HirudineeADRT <hirudinee@adroitlogic.com>
2019-08-01 11:39:18 +0530

    remove rds

--- hiru0108_issue667/lambda.js
+++ hiru0108_issue667/lambda.js
@@ -11,22 +11,6 @@ exports.handler = function (event, context, callback) {
     // You can pass the existing connection to this function.
     // A new connection will be created if it's not present as the third param 
     // You must always end/destroy the DB connection after it's used
-    rds.query({
-        instanceIdentifier: 'hirudinee',
-        query: 'insert into test (id)',
-        inserts: [1]
-    }, function (error, results, connection) {
-        if (error) {
-            console.log("Error occurred");
-            throw error;
-        } else {
-            console.log("Success")
-            console.log(results);
-        }
-
-        connection.end();
-    });
-
-
+ 
     callback(null, { "message": "Successfully executed" });
 }
